a753f6345654b4cacd5369349452b85ef68f5ab9
Monam1083 <monammajeed1083@gmail.com>
2026-08-13 22:04:08 +0500

    update

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
