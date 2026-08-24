b64fb42fbaa6f2c470831f750d919bcf838ff9b1
Monam1083 <monammajeed1083@gmail.com>
2026-08-24 22:10:30 +0500

    getmaterial app

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: HomePage(),
