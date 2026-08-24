1173bf65f2dedc84e3b1194534c41b94b38897be
Monam1083 <monammajeed1083@gmail.com>
2026-08-24 22:28:09 +0500

    center alignment

--- lib/Page/home_page.dart
+++ lib/Page/home_page.dart
@@ -14,6 +14,7 @@ class HomePage extends StatelessWidget {
           child: Column(
             children: [
               Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   ElevatedButton(
                     onPressed: () {
