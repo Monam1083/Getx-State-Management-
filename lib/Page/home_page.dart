b03e144ba3dcc0c4bfc6876a4d641332a29e1fc1
Monam1083 <monammajeed1083@gmail.com>
2026-08-24 22:02:10 +0500

    add column widg

--- lib/Page/home_page.dart
+++ lib/Page/home_page.dart
@@ -5,6 +5,8 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text("Home Page")));
+    return Scaffold(
+      body: Column(children: [Center(child: Text("Home Page"))]),
+    );
   }
 }
