57a96674b176353e906307c9d88010c096a5bbe1
Monam1083 <monammajeed1083@gmail.com>
2026-08-24 22:35:58 +0500

    add app bar

--- lib/Page/home_page.dart
+++ lib/Page/home_page.dart
@@ -8,6 +8,7 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.all(8.0),
