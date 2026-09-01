6afea3cd7ed2634762372be8d2aa5dfa0db12b03
Monam1083 <monammajeed1083@gmail.com>
2026-09-01 22:59:25 +0500

    update

--- lib/Page/home_page.dart
+++ lib/Page/home_page.dart
@@ -4,7 +4,6 @@ import 'package:get/get_navigation/get_navigation.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'package:getx_practice_application/Page/home_page.dart';
-
 void main() {
   runApp(const MyApp());
 }
