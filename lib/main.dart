4edaa96077a00cb298d36cffb42637fa2e78bbf5
Monam1083 <monammajeed1083@gmail.com>
2026-09-04 22:39:12 +0500

    update title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Getx ',
       theme: ThemeData(colorScheme: .fromSeed(seedColor: Colors.deepPurple)),
       home: HomePage(),
     );
